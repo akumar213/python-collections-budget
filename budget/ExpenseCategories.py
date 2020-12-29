7bdf85f795b4265ac0d7002b029cfcc44017906c
anandkumar <anand213@gmail.com>
2020-12-29 09:26:18 -0500

    update code for check pass

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -26,13 +26,23 @@ expenses.read_expenses('data/spending_data.csv')
 '''
 
     print(timeit.timeit(stmt='expenses.categorize_for_loop()',
-                        setup=setup_code,
+                        setup=
+                        '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                        ''',
                         number=100000,
                         globals=globals()
                         ))
 
     print(timeit.timeit(stmt='expenses.categorize_set_comprehension()',
-                        setup=setup_code,
+                        setup=
+                        '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                        ''',
                         number=100000,
                         globals=globals()
                         ))
